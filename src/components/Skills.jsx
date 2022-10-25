45d6a895224ce30f5a917cd7d5146a4be94a7777
rey genosa <genosarey16@gmail.com>
2022-10-25 22:56:13 +0800

    first commit

---
+++ src/components/Skills.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+
+import HTML from "../assets/html.png";
+import CSS from "../assets/css.png";
+import Javascript from "../assets/javascript.png";
+import ReactImg from "../assets/react.png";
+import Node from "../assets/node.png";
+import AWS from "../assets/aws.png";
+import Github from "../assets/github.png"
+import Tailwind from "../assets/tailwind.png"
+
+const Skills = () => {
+  return (
+    <div name="skills" className="w-full h-screen bg-[#0a192f] text-gray-300">
+    {/* Container */}
+    <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div>
+        <p className="text-4xl font-bold inline border-b-4 border-pink-600">Skills</p>
+        <p className="py-4">These are the technologies I've worked with</p>
+        </div>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                <img className="w-20 mx-auto" src={HTML} alt="HTML icon" />
+                <p>html</p>
+            </div>
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                <img className="w-20 mx-auto" src={CSS} alt="CSS icon" />
+                <p>CSS</p>
+            </div>
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                <img className="w-20 mx-auto" src={Javascript} alt="Javascript icon" />
+                <p>Javascript</p>
+            </div>
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                <img className="w-20 mx-auto" src={ReactImg} alt="React icon" />
+                <p>React</p>
+            </div>
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                <img className="w-20 mx-auto" src={AWS} alt="AWS icon" />
+                <p>AWS</p>
+            </div>
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                <img className="w-20 mx-auto" src={Github} alt="Github icon" />
+                <p>Github</p>
+            </div>
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                <img className="w-20 mx-auto" src={Tailwind} alt="Tailwind icon" />
+                <p>Tailwind</p>
+            </div>
+            <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                <img className="w-20 mx-auto" src={Node} alt="Node icon" />
+                <p>Node</p>
+            </div>
+        
+        </div>
+    </div>
+    </div>
+  )
+}
+
+export default Skills
