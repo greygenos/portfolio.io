6fbb856eb0fdbf920ce8bde8b7ba09b04ec6fd35
rey genosa <genosarey16@gmail.com>
2022-10-27 21:21:12 +0800

    additional features

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import {HiArrowNarrowRight} from "react-icons/hi"
+
 const Home = () => {
   return (
-    <div  name= "Home" className = "w-full h-screen bg-[#0a192f]">
+    <div  name= "home" className = "w-full h-screen bg-[#0a192f]">
 
         {/* Container */}
         <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -37,7 +37,7 @@ Local website
                     <div className="opacity-0 group-hover:opacity-100">
                         <span className='text-2xl font-bold text-white tracking-wider'>
 React Counter
-                        </span>
+                           </span>
                         <div className='pt-8 text-center'>
                             <a href="/">
                                 <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">Demo</button>
