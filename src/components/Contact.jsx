eb0377541767069ced35304482d2a81240bb4229
rey genosa <genosarey16@gmail.com>
2022-11-02 19:20:16 +0800

    getform contact added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name="contact" className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4">
-        <form method="POST" action="" className="flex flex-col max-w-[600px] w-full">
+        <form method="POST" action="https://getform.io/f/286da01b-016d-4fb8-b2ed-2dd35f8e38d9" className="flex flex-col max-w-[600px] w-full">
             <div className="pb-8 ">
                 <p className="text-4xl font-bold inline border-b-4 border-pink-600 text-gray-100">Contact</p>
                 <p className="py-6 text-gray-300">Lets work together</p>
