489c510af8e69cb2636376126d4c92d8bdc84878
greygenos <genosarey16@gmail.com>
2024-03-31 14:45:47 +0800

    Changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,11 +12,7 @@ const About = () => {
             </div> 
             <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
                 <div className= "sm:text-right text-4xl font-bold">
-<<<<<<< HEAD
-                    <p>Hi. I am Rey Genosa. A Junior Developer from the Philippines. Nice meeting you here. Please take a look around</p>
-=======
-                    <p>Hi. I am Rey Genosa. Nice meeting you here. Please take a look around</p>
->>>>>>> 30f3a5853782203a8a077f8a2bcec16ee8bd40d1
+            <p> Hi. I am Rey Genosa. Nice meeting you here. Please take a look around</p>
                 </div>
                 <div>
                     <p>
